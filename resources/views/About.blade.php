195a1c518ab80d3e9511735655079819e9ad01d0
FeDeRiCo9719 <federico.lovaglio.1997@gmail.com>
2021-11-06 17:30:56 +0100

    style

--- resources/views/About.blade.php
+++ resources/views/About.blade.php
@@ -3,6 +3,8 @@
 @section('title', 'About')
 
 @section('content')
-    <h2>About Us</h2>
-    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni sit corporis vitae officia suscipit adipisci alias ullam, dicta tempora quia tenetur natus minima, eius vel? Vitae ipsam perferendis odio ad ipsum dolor sit amet consectetur adipisicing elit. Consectetur dolorem rem dicta soluta debitis alias minus quod aliquam quidem, voluptatibus, quos excepturi exercitationem, dolore sed. Nostrum est dolorum fugit dolor.</p>
+    <div class="aboutBox">
+        <h2>About Us</h2>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni sit corporis vitae officia suscipit adipisci alias ullam, dicta tempora quia tenetur natus minima, eius vel? Vitae ipsam perferendis odio ad ipsum dolor sit amet consectetur adipisicing elit. Consectetur dolorem rem dicta soluta debitis alias minus quod aliquam quidem, voluptatibus, quos excepturi exercitationem, dolore sed. Nostrum est dolorum fugit dolor.</p>
+    </div>
 @endsection
